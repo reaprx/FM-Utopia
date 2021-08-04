9dcb67f8d447da6cbd84e375425e70bd8625bba6
Hasalanka Seneviratne <seneviratnehasalanka@gmail.com>
2021-08-04 21:43:38 +0530

    Add files via upload

---
+++ playback.js
@@ -0,0 +1,63 @@
+const sources = [
+  "http://stream.zeno.fm/ffzk298hx38uv", // sinhala
+  "http://stream.zeno.fm/fgrwvmd8u18uv", // english
+];
+
+const labels = [
+  [ "ReaprX Online Radio Station", "https://www.fm.reaprx.tk" ],
+  [ "ReaprX Online Radio Station", "https://www.fm.reaprx.tk"],
+];
+
+let playingIndex = 0; // current radio
+let playing = true; // stream status
+let music = null;
+
+setTimeout(function(){
+    loadStream(playingIndex);
+}, 1);
+
+function loadStream(index){
+  if(playing && music !== null)
+    destroyStream();
+
+  music = new Audio();
+  music.src = sources[index];
+  music.load();
+  music.play();
+  setLabel(index);
+  pauseIcon();
+  playingIndex = index;
+  playing = true;
+}
+
+function destroyStream(){
+  music.pause();
+  music.src = "";
+  playIcon();
+  playing = false;
+}
+
+function changePlayback(){
+  if(playing){ destroyStream();  }else{  loadStream(playingIndex);  }
+}
+
+function setLabel(index){
+  document.getElementById("label").innerHTML = '<h6> <a target="_blank" href="' + labels[index][1] + '">' + labels[index][0] + '</a></h6>';
+}
+
+document.onkeydown = function(e) {
+  e = e || window.event;
+  switch(e.which || e.keyCode) {
+    case 32:
+      changePlayback();
+    break;
+  }
+};
+
+// icons
+function pauseIcon(){
+  document.getElementById('playbackButton').className = 'icon fa-pause';
+}
+function playIcon(){
+  document.getElementById('playbackButton').className = 'icon fa-play';
+}
